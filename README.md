dff07adfed3e3f480b83e29408f5eb1216407c47
Steve Dougherty <steve@asksteved.com>
2020-02-14 22:46:56 -0500

    Print removed duplicate count

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # duplicate-picture-finder
- Find duplicate pictures in a directory
+ Find duplicate files in a directory
 
-I made this after I accidentally used a different form of Windows photo import that gave me duplicates of each photo it processed.
+I made this after I accidentally used a different form of Windows photo import that gave me duplicates of each photo it
+processed.
+
+## Usage:
+
+List duplicates based on filename - `IMG_E0778 (1).JPG` is a duplicate of `IMG_E0778.JPG`. 
+```shell script
+    python duplicate-picture-finder.py [path-to-directory]
+``` 
+
+List duplicates based on file content hash:
+```shell script
+    python duplicate-picture-finder.py --find-by=file_name [path-to-directory]
+```
+
+To remove these duplicates instead of printing each path, add `--remove-duplicates`.
+
+See `--help` for more information.
--- duplicate_picture_finder/__main__.py
+++ duplicate_picture_finder/__main__.py
@@ -3,6 +3,7 @@ import argparse
 import hashlib
 import os
 import re
+import sys
 
 picture_types = [
     ".jpg",
@@ -28,23 +29,33 @@ def main():
                              "occurrence of a given picture.")
     parser.add_argument("--find-by", default="filename",
                         choices=backends.keys())
+    parser.add_argument("--pictures-only", action="store_true",
+                        help="If specified, only consider files with picture "
+                             "filename extensions.")
     args = parser.parse_args()
 
-    for paths in backends[args.find_by](args.path):
-        if len(paths) > 1:
-            print(f"Found in {len(paths)} places:")
-            for path in paths:
-                print(f"  {path}")
+    # os.walk() does not raise an exception if its initial path does not exist.
+    if not os.path.exists(args.path):
+        print(f"Error: starting path {os.path.realpath(args.path)} not found.")
+        exit(1)
 
+    for paths in backends[args.find_by](args.path, args.pictures_only):
+        if len(paths) > 1:
             if args.remove_duplicates:
                 shortest_path = min(paths, key=lambda s: len(s))
                 paths.remove(shortest_path)
+                print(f"Removing {len(paths)} duplicates of "
+                      f"{os.path.realpath(shortest_path)}")
 
                 for path in paths:
                     os.remove(path)
+            else:
+                print(f"Found in {len(paths)} places:")
+                for path in paths:
+                    print(f"  {path}")
 
 
-def find_by_filename(starting_path):
+def find_by_filename(starting_path, pictures_only):
     # Number in parenthesis just before extension.
     numbered_file = re.compile(r" \(\d+\)\.[^.]+$")
     extension = re.compile(r"\.[^.]+$")
@@ -52,6 +63,12 @@ def find_by_filename(starting_path):
     names = collections.defaultdict(list)
     for dirpath, dirnames, filenames in os.walk(starting_path):
         for filename in filenames:
+            if pictures_only:
+                # Only consider files with an extension type.
+                if not any(map(lambda s: filename.lower().endswith(s),
+                               picture_types)):
+                    continue
+
             match = numbered_file.search(filename)
             if match:
                 # That before the number if one is given.
@@ -67,14 +84,15 @@ def find_by_filename(starting_path):
     return names.values()
 
 
-def find_by_file_hash(starting_path):
+def find_by_file_hash(starting_path, pictures_only):
     hashes = collections.defaultdict(list)
     for dirpath, dirnames, filenames in os.walk(starting_path):
         for filename in filenames:
-            # Only consider files with an extension type.
-            if not any(map(lambda s: filename.lower().endswith(s),
-                           picture_types)):
-                continue
+            if pictures_only:
+                # Only consider files with an extension type.
+                if not any(map(lambda s: filename.lower().endswith(s),
+                               picture_types)):
+                    continue
 
             file_path = os.path.join(dirpath, filename)
             with open(file_path, "rb") as infile:
