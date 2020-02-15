88e53401adb0fafaf3e54d490e0b17024a229aaf
Steve Dougherty <steve@asksteved.com>
2020-02-14 22:12:34 -0500

    Add filename-based duplicate finding

--- duplicate_picture_finder/__main__.py
+++ duplicate_picture_finder/__main__.py
@@ -2,6 +2,7 @@ import collections
 import argparse
 import hashlib
 import os
+import re
 
 picture_types = [
     ".jpg",
@@ -14,15 +15,22 @@ picture_types = [
 
 
 def main():
+    backends = {
+        "file_hash": find_by_file_hash,
+        "filename": find_by_filename,
+    }
+
     parser = argparse.ArgumentParser()
     parser.add_argument("path", nargs="?", default=".",
                         help="Directory to find duplicate pictures in.")
     parser.add_argument("--remove-duplicates", action="store_true",
                         help="If specified, delete all but the shortest-named "
                              "occurrence of a given picture.")
+    parser.add_argument("--find-by", default="filename",
+                        choices=backends.keys())
     args = parser.parse_args()
 
-    for file_hash, paths in find_duplicate_paths(args.path).items():
+    for paths in backends[args.find_by](args.path):
         if len(paths) > 1:
             print(f"Found in {len(paths)} places:")
             for path in paths:
@@ -36,7 +44,30 @@ def main():
                     os.remove(path)
 
 
-def find_duplicate_paths(starting_path):
+def find_by_filename(starting_path):
+    # Number in parenthesis just before extension.
+    numbered_file = re.compile(r" \(\d+\)\.[^.]+$")
+    extension = re.compile(r"\.[^.]+$")
+
+    names = collections.defaultdict(list)
+    for dirpath, dirnames, filenames in os.walk(starting_path):
+        for filename in filenames:
+            match = numbered_file.search(filename)
+            if match:
+                # That before the number if one is given.
+                base_filename = filename[:match.start()]
+            else:
+                # Otherwise that before the extension.
+                extension_match = extension.search(filename)
+                base_filename = filename[:extension_match.start()]
+
+            file_path = os.path.join(dirpath, filename)
+            names[base_filename].append(file_path)
+
+    return names.values()
+
+
+def find_by_file_hash(starting_path):
     hashes = collections.defaultdict(list)
     for dirpath, dirnames, filenames in os.walk(starting_path):
         for filename in filenames:
@@ -51,4 +82,4 @@ def find_duplicate_paths(starting_path):
 
             hashes[file_hash].append(file_path)
 
-    return hashes
+    return hashes.values()
